eeffd26040e81e434e4c2c1e2969ced6af8a2a9b
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2021-03-28 20:25:58 +0200

    Working

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,107 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <Home />
+    <div class="col">
+      <h1 class="title">Currency Converter</h1>
+    </div>
+    <div>
+      <div class="">
+        <input
+          id="currencyInput"
+          class="currency-input"
+          v-model="amountToConvert"
+          @keypress="isNumber"
+          @input="onInput"
+          placeholder="Enter Amount to Convert"
+        />
+      </div>
+      <select v-model="base">
+        <option v-for="(item, index) in rates" :key="index">
+          {{ item }}
+        </option>
+      </select>
+      to
+      <select v-model="currencyTo">
+        <option v-for="(item, index) in rates" :key="index">
+          {{ item }}
+        </option>
+      </select>
+    </div>
+    <div>
+      {{ amountToConvert }} {{ base }}
+      =
+      {{ convertedAmount }}
+      {{ currencyTo }}
+    </div>
   </div>
 </template>
 
 <script>
-import Home from "./components/Home.vue";
+import axios from "axios";
 
 export default {
   name: "App",
-  components: {
-    Home,
+  components: {},
+  computed: {
+    rates: {
+      get: function() {
+        return this.rates_internal ? Object.keys(this.rates_internal) : [];
+      },
+      set: function(value) {
+        this.rates_internal = value;
+      },
+    },
+  },
+  data() {
+    return {
+      last_convertions: [],
+      rates_internal: null,
+      base: "USD",
+      currencyTo: "PLN",
+      amountToConvert: "",
+      convertedAmount: "",
+    };
+  },
+  methods: {
+    isNumber: function(evt) {
+      evt = evt ? evt : window.event;
+      let charCode = evt.which ? evt.which : evt.keyCode;
+      if (
+        charCode > 31 &&
+        (charCode < 48 || charCode > 57) &&
+        charCode !== 46
+      ) {
+        evt.preventDefault();
+        return false;
+      } else {
+        return true;
+      }
+    },
+    onInput: function() {
+      axios
+        .get(
+          `https://api.exchangeratesapi.io/latest?base=${this.base}&symbols=${this.currencyTo}`
+        )
+        .then((response) => {
+          let exchangeRate = Object.values(response.data.rates)[0];
+          this.convertedAmount = (exchangeRate * this.amountToConvert).toFixed(
+            2
+          );
+        });
+    },
+  },
+  mounted() {
+    axios
+      .get("https://api.exchangeratesapi.io/latest")
+      .then((response) => {
+        this.rates_internal = response.data.rates ? response.data.rates : [];
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+  watch: {
+    base: "onInput",
+    currencyTo: "onInput",
   },
 };
 </script>
