a6aff8c0aaf59282479db3741101ac8943a54884
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2021-03-29 13:37:23 +0200

    Add sass Styles

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,40 @@
 <template>
   <div id="app">
-    <div class="col">
-      <h1 class="title">Currency Converter</h1>
-    </div>
-    <div>
-      <div class="">
-        <input
-          class="currency-input"
-          v-model="amountToConvert"
-          @keypress="isNumber"
-          @input="onInputDebounced"
-          placeholder="Enter Amount to Convert"
-        />
+    <h1 class="title">Currency Converter</h1>
+    <div class="container">
+      <input
+        class="currency-input"
+        v-model="amountToConvert"
+        @keypress="isNumber"
+        @input="onInputDebounced"
+        placeholder="Enter Amount to Convert"
+      />
+
+      <select class="select base" v-model="base">
+        <option v-for="(item, index) in rates" :key="index">
+          {{ item }}
+        </option>
+      </select>
+      >
+      <select class="select currencyTo" v-model="currencyTo">
+        <option v-for="(item, index) in rates" :key="index">
+          {{ item }}
+        </option>
+      </select>
+      <div class="result">
+        {{ amountToConvert }} {{ base }}
+        =
+        {{ convertedAmount }}
+        {{ currencyTo }}
       </div>
-      <select v-model="base">
-        <option v-for="(item, index) in rates" :key="index">
-          {{ item }}
-        </option>
-      </select>
-      to
-      <select v-model="currencyTo">
-        <option v-for="(item, index) in rates" :key="index">
-          {{ item }}
-        </option>
-      </select>
     </div>
-    <div class="">
-      {{ amountToConvert }} {{ base }}
-      =
-      {{ convertedAmount }}
-      {{ currencyTo }}
-    </div>
-    <div v-for="(convertion, index) in latestConvertionsReverse" :key="index">
-      ({{ convertion.date }}) : {{ convertion.amountToConvert }}
+    <h3 class="history">Currency Converter History</h3>
+    <div
+      class="result-history"
+      v-for="(convertion, index) in latestConvertionsReverse"
+      :key="index"
+    >
+      ({{ convertion.date }}): {{ convertion.amountToConvert }}
       {{ convertion.base }} = {{ convertion.convertedAmount }}
       {{ convertion.currencyTo }}
     </div>
@@ -123,13 +125,73 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #000000;
   margin-top: 60px;
+  font-size: 24px;
+}
+
+.container {
+  -ms-flex: 0 0 66.666667%;
+  flex: 0 0 66.666667%;
+  max-width: 66.666667%;
+  border-radius: 30px;
+  text-align: center;
+  background: rgb(248, 248, 248);
+  margin: auto;
+}
+@media (max-width: 768px) {
+  .container {
+    margin: 0px;
+    max-width: 100%;
+  }
+}
+.currency-input {
+  height: 50px;
+  font-size: 100%;
+  font-weight: bold;
+  border: 0;
+  // padding: 7px 15px;
+  border: 1px solid #ccc;
+  position: relative;
+  background: transparent;
+  margin: 5px 10px;
+}
+.result {
+  text-align: center;
+  font-weight: bold;
+  margin-top: 15px;
+}
+h1,
+h3 {
+  margin-left: 1.3em;
+}
+.result-history {
+  margin-top: 15px;
+  margin-left: 1.3em;
+  border-bottom: 1px solid #2c3e50;
+}
+select {
+  height: 50px;
+  font-size: 100%;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 0;
+  background: transparent;
+  border: none;
+  border-bottom: 2px solid #2b2424;
+  color: rgb(0, 0, 0);
+  appearance: none;
+  padding: 10px;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+}
+/* For IE <= 11 */
+select::-ms-expand {
+  display: none;
 }
 </style>
